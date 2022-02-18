7031524735a53e9546caa1bbfdd8dacac98dd0f8
k-yamaki <k.yamaki@hiroshimamm.com>
2022-02-18 17:05:24 +0900

    Initial Commit

---
+++ Sample62-MapLaunch/Sample62_MapLaunchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sample62_MapLaunchApp.swift
+//  Sample62-MapLaunch
+//
+//  Created by keiji yamaki on 2022/02/18.
+//
+
+import SwiftUI
+
+@main
+struct Sample62_MapLaunchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
