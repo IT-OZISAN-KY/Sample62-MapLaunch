307d0510e0ab14a9fd36b522d1e1e852a868e15c
k-yamaki <k.yamaki@hiroshimamm.com>
2022-02-18 17:30:30 +0900

    地図アプリの起動

--- Sample62-MapLaunch/ContentView.swift
+++ Sample62-MapLaunch/ContentView.swift
@@ -9,8 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("地図アプリ起動"){
+            // 開始：瀬野駅
+            let startLatitude = 34.42227024800514
+            let startLongitude = 132.59667818752467
+            // 終点：HMM
+            let destinationLatitude = 34.22654972624221
+            let destinationLongitude = 132.63116074016824
+            // let url = URL(string: "maps://?saddr=&daddr=\(latitude),\(longitude)")
+            let url = URL(string: "maps://?saddr=\(startLatitude),\(startLongitude)&daddr=\(destinationLatitude),\(destinationLongitude)")
+
+            if UIApplication.shared.canOpenURL(url!) {
+                  UIApplication.shared.open(url!, options: [:], completionHandler: nil)
+            }
+        }
     }
 }
 
